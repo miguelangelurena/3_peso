d59fbaa748d354e0e3d3e4a74299af31075607fd
Miguel Urena <anguelmiguel31@gmail.com>
2024-12-07 17:40:29 -0400

    feat: add youtube banner & fix banner section

---
+++ src/components/islands/Counter.tsx
@@ -0,0 +1,42 @@
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+/**
+ *
+ * @param root0
+ * @param root0.value
+ */
+
+interface Props {
+  value: number;
+  direction?: "up" | "down";
+  className?: string;
+}
+
+export default function Counter({ value, direction = "up", className }: Props) {
+  const ref = useRef<HTMLSpanElement>(null);
+  const motionValue = useMotionValue(direction === "down" ? value : 0);
+  const springValue = useSpring(motionValue, {
+    damping: 100,
+    stiffness: 100,
+  });
+  const isInView = useInView(ref, { once: true, margin: "-100px" });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(direction === "down" ? 0 : value);
+    }
+  }, [motionValue, isInView]);
+
+  useEffect(
+    () =>
+      springValue.on("change", (latest) => {
+        if (ref.current) {
+          ref.current.textContent = latest.toFixed(0);
+        }
+      }),
+    [springValue],
+  );
+
+  return <span className={className} ref={ref} />;
+}
