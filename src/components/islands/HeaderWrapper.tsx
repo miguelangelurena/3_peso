970a15240b29346377c90aac6d98b693bfc38334
Isaac Martinez <isaacismaelx14@gmail.com>
2024-12-14 18:21:15 -0400

    refactor(footer, header): improve layout and styling for footer and header components
    - Updated Footer.astro to use a more concise structure and responsive utility classes
    - Adjusted footer image size for better responsiveness
    - Enhanced HeaderWrapper.tsx styling with updated background and z-index for improved visual hierarchy

--- src/components/islands/HeaderWrapper.tsx
+++ src/components/islands/HeaderWrapper.tsx
@@ -10,7 +10,7 @@ const HeaderWrapper: React.FC<HeaderWrapperProps> = ({ children }) => {
 
   return (
     <motion.header
-      className="fixed top-0 z-10 w-full border-b border-slate-800 bg-slate-800/25 py-1 backdrop-blur-md transition-all"
+      className="fixed top-0 z-30 w-full border-b border-slate-800 bg-dark/85 py-1 backdrop-blur-md transition-all"
       initial={{ y: "-100px" }}
       animate={{ y: heroVideoLoaded ? "0" : undefined }}
       exit={{ y: "-100px" }}
