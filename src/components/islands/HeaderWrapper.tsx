0745b2d015dfe1a1506510b4a551e353edbf6e27
Isaac Martinez <isaacismaelx14@gmail.com>
2024-12-17 14:46:04 -0400

    feat: add new privacy policy and terms pages with updated layout and animations
    - Add `privacy-policy.astro` and `terms-and-conditions.astro` pages.
    - Enhance `HeaderWrapper` to support header animation.
    - Update `BaseLayout` with new props for dynamic backgrounds and header animations.
    - Improve footer content by adding privacy policy and terms links.
    - Adjust navigation links in `header.astro` for consistent routing.
    - Enhance `Contact.astro` with additional description.

--- src/components/islands/HeaderWrapper.tsx
+++ src/components/islands/HeaderWrapper.tsx
@@ -3,17 +3,24 @@ import type { PropsWithChildren } from "react";
 
 import { useAppStore } from "./stores/useAppStore";
 
-type HeaderWrapperProps = PropsWithChildren;
+type HeaderWrapperProps = PropsWithChildren & {
+  headerAnimation?: boolean;
+};
 
-const HeaderWrapper: React.FC<HeaderWrapperProps> = ({ children }) => {
+const HeaderWrapper: React.FC<HeaderWrapperProps> = ({
+  children,
+  headerAnimation = true,
+}) => {
   const { heroVideoLoaded } = useAppStore();
 
   return (
     <motion.header
-      className="fixed top-0 z-30 w-full border-b border-slate-800 bg-dark/85 px-8 py-1 backdrop-blur-md transition-all"
-      initial={{ y: "-100px" }}
-      animate={{ y: heroVideoLoaded ? "0" : undefined }}
-      exit={{ y: "-100px" }}
+      className="fixed top-0 z-30 w-full border-b border-slate-800 bg-dark/85 px-8 py-1 text-white backdrop-blur-md transition-all"
+      initial={headerAnimation ? { y: "-100px" } : undefined}
+      animate={
+        headerAnimation ? { y: heroVideoLoaded ? "0" : undefined } : undefined
+      }
+      exit={headerAnimation ? { y: "-100px" } : undefined}
     >
       {children}
     </motion.header>
