ab1045f2e32409c71631aeceecfe294f459d2a31
Miguel <anguelmiguel31@gmail.com>
2026-05-07 09:54:53 -0400

    Switch Astro adapter to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,17 @@
 // @ts-check
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  server: {
-    host: "0.0.0.0",
-  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
   ],
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
